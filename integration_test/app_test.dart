b3f9e08236b78583c35718d82ef6ba3a066551fc
11ange <luis.lange@gmail.com>
2025-09-24 20:39:28 -0300

    teste

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -11,7 +11,7 @@ void main() {
         (WidgetTester tester) async {
       
       app.main();
-      await tester.pumpAndSettle(const Duration(seconds: 3));
+      await tester.pumpAndSettle(const Duration(seconds: 7));
 
       // 2. TELA DE LOGIN
       final emailField = find.byKey(const Key('email_field'));
@@ -23,46 +23,45 @@ void main() {
       await tester.enterText(passwordField, '123456');
       await tester.pumpAndSettle();
       await tester.tap(loginButton);
+      await tester.pumpAndSettle(const Duration(seconds: 10));
+
+      // 3. TELA INICIAL (HOME)
+      await tester.tap(find.text('Pacientes'));
       await tester.pumpAndSettle();
-      //await tester.pumpAndSettle(const Duration(seconds: 5));
 
-      // // 3. TELA INICIAL (HOME)
-      // await tester.tap(find.text('Pacientes'));
-      // await tester.pumpAndSettle();
+      // 4. TELA DE PACIENTES ATIVOS
+      await tester.tap(find.byKey(const Key('novo_paciente_button')));
+      await tester.pumpAndSettle();
 
-      // // 4. TELA DE PACIENTES ATIVOS
-      // await tester.tap(find.byKey(const Key('novo_paciente_button')));
-      // await tester.pumpAndSettle();
+      // 5. TELA DE FORMULÁRIO DE PACIENTE
+      final pacienteNome = 'Paciente Web Teste ${DateTime.now().millisecondsSinceEpoch}';
 
-      // // 5. TELA DE FORMULÁRIO DE PACIENTE
-      // final pacienteNome = 'Paciente Teste ${DateTime.now().millisecondsSinceEpoch}';
-
-      // final nomeField = find.byKey(const Key('paciente_nome_field'));
-      // final responsavelField = find.byKey(const Key('paciente_responsavel_field'));
-      // final dataNascimentoFieldKey = const Key('paciente_data_nascimento_field');
-      // final cadastrarButton = find.byKey(const Key('cadastrar_paciente_button'));
+      final nomeField = find.byKey(const Key('paciente_nome_field'));
+      final responsavelField = find.byKey(const Key('paciente_responsavel_field'));
+      final dataNascimentoFieldKey = const Key('paciente_data_nascimento_field');
+      final cadastrarButton = find.byKey(const Key('cadastrar_paciente_button'));
       
-      // await tester.enterText(nomeField, pacienteNome);
-      // await tester.enterText(responsavelField, 'Responsável Teste');
+      await tester.enterText(nomeField, pacienteNome);
+      await tester.pumpAndSettle();
+      await tester.enterText(responsavelField, 'Responsável Teste Web');
+      await tester.pumpAndSettle();
       
-      // // --- AJUSTE FINAL AQUI ---
-      // // Tocamos no campo de data e ignoramos o warning, pois sabemos que funciona.
-      // await tester.tap(find.byKey(dataNascimentoFieldKey), warnIfMissed: false);
-      // await tester.pumpAndSettle();
+      await tester.tap(find.byKey(dataNascimentoFieldKey), warnIfMissed: false);
+      await tester.pumpAndSettle();
       
-      // await tester.tap(find.text('OK'));
-      // await tester.pumpAndSettle();
+      await tester.tap(find.text('OK'));
+      await tester.pumpAndSettle();
 
-      // await tester.ensureVisible(cadastrarButton);
-      // await tester.tap(cadastrarButton);
+      await tester.ensureVisible(cadastrarButton);
+      await tester.tap(cadastrarButton);
 
-      // await tester.pumpAndSettle(const Duration(seconds: 5));
+      await tester.pumpAndSettle(const Duration(seconds: 15));
 
-      // // 6. VERIFICAÇÃO FINAL
-      // expect(find.text('Pacientes Ativos'), findsOneWidget);
-      // expect(find.text(pacienteNome), findsOneWidget);
+      // 6. VERIFICAÇÃO FINAL
+      expect(find.text('Pacientes Ativos'), findsOneWidget);
+      expect(find.text(pacienteNome), findsOneWidget);
       
-      // debugPrint('SUCESSO: Paciente "$pacienteNome" criado e verificado com sucesso!');
+      debugPrint('SUCESSO: Paciente "$pacienteNome" criado e verificado!');
     });
   });
 }
